8e3b83166ff7ba352181e0bd7bf2ba72a29da1de
akmalaltaf333 <akmalaltaf692@gmail.com>
2025-12-18 07:33:38 -0800

    Add files via upload

---
+++ lab 2/tasks 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//cout<<"welcome to the C++ labe!"<<endl;
+	//cout<<"you will learn output today"<<endl;
+	cout<<"*"<<endl;
+	cout<<"* *"<<endl;
+	cout<<"* * *"<<endl;
+	cout<<"* * * *"<<endl;
+	return 0;
+	
+}
